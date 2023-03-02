39555ce2d6e9f8c7adaa6ee7330f9bf477a72643
Kübra TAŞKIN <125082472+113737@users.noreply.github.com>
2023-03-02 17:46:07 +0300

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,28 @@
+package day04_Variables;
+
+public class Circle {
+
+    public static void main(String[] args) {
+        double PI = 3.14;
+        double radius = 3.5;
+        double diameter = 2 * radius ;
+        double area = radius * radius * PI ;
+        double perimeter = 2 * radius * PI ;
+
+        System.out.println("perimeter = " + perimeter);
+        System.out.println("area = " + area);
+        System.out.println("diameter = " + diameter);
+        System.out.println("radius = " + radius);
+        System.out.println("PI = " + PI);
+
+
+    }
+}
+/*
+3. Create a class named Circle, write a program that can calculate the area & perimeter of any given Circle
+			PI, radius, diameter, area, perimeter
+
+					Hints: 	PI = 3.14
+							area = radius * radius * PI
+							Perimeter = 2 * radius * PI
+ */
---
+++ Currencies.java
@@ -0,0 +1,29 @@
+package day04_Variables;
+
+public class Currencies {
+    public static void main(String[] args) {
+
+
+        int dollar =1000;
+
+        double lira = dollar * 18.88;
+        double euro = dollar * 0.94;
+        double JPY = dollar * 136.42;
+        double pound = dollar * 0.82;
+        double CAD = dollar * 1.36;
+
+
+        System.out.println("dollar = " + dollar);
+        System.out.println("lira = " + lira);
+        System.out.println("euro = " + euro);
+        System.out.println("JPY = " + JPY);
+        System.out.println("pound = " + pound);
+        System.out.println("CAD = " + CAD);
+
+
+
+
+
+
+    }
+}
